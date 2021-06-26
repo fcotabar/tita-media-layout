67f8de99d86907d4eec9dd0f41ae9ebd95a728a8
fcotabar <fcotabar@gmail.com>
2021-06-26 17:29:07 -0500

    Grid implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,14 +7,61 @@ const ITEMS_PER_PAGE = 9;
 let page = 1;
 
 const portfolioGrid = document.querySelector('.portfolio__grid');
+const btnShowMore = document.querySelector('.btn__show-more');
 
-const renderPhotos = function (img, title) {
+const showMoreHandler = function (e) {
+  e.preventDefault();
+  console.log(e.target);
+  console.log('handle event');
+  page++;
+  // console.log(page);
+  loadPhotos(
+    `${API_URL}query=london&page=${page}&per_page=${ITEMS_PER_PAGE}&client_id=${API_KEY}`
+  );
+};
+// console.log(page);
+
+const getItemSize = function (height, width) {
+  const sizes = [
+    'item-size--small-0',
+    'item-size--small-1',
+    'item-size--small-2',
+    'item-size--small-3',
+    'item-size--small-4',
+    'item-size--small-5',
+    'item-size--small-6',
+    'item-size--small-7',
+    'item-size--small-8',
+    'item-size--small-9',
+    'item-size--medium-0',
+    'item-size--medium-1',
+    'item-size--medium-2',
+    'item-size--medium-3',
+    'item-size--medium-4',
+    'item-size--medium-5',
+    'item-size--medium-6',
+    'item-size--medium-7',
+    'item-size--medium-8',
+    'item-size--medium-9',
+  ];
+
+  return sizes[Math.floor((height / width) * 10)];
+};
+
+const renderPhotos = function (imgSrc, imgTitle, imgSize) {
   return `
+  <div class="portfolio__grid-item ${imgSize}">
   <img
-    class="portfolio__grid-item"
-    src="${img}"
-    alt="${title}"
+  class="portfolio__grid-item-img"
+  src="${imgSrc}"
+  alt="${imgTitle}"
   />
+  <div class="portfolio__branding">
+  <h3 class="portfolio__grid-item-title">CREATIVE LOGO</h3>
+  <p class="portfolio__grid-item-text">Branding</p>
+</div>
+
+  </div>
   `;
 };
 
@@ -23,7 +70,16 @@ const loadPhotos = async function (url) {
     const fetchData = await fetch(url);
     const { results } = await fetchData.json();
     results.map((photo) => {
-      const markup = renderPhotos(photo.urls.thumb, photo.alt_description);
+      const { urls, alt_description, height, width } = photo;
+      const itemSize = getItemSize(height, width);
+
+      console.log(
+        `H: ${height}- W: ${width}=${height / width} ==> ${Math.floor(
+          (height / width) * 10
+        )} =>>> ${itemSize}`
+      );
+
+      const markup = renderPhotos(urls.small, alt_description, itemSize);
       portfolioGrid.insertAdjacentHTML('beforeend', markup);
     });
     // .insertAdjacentHTML('afterbegin', markup)
@@ -35,8 +91,9 @@ const loadPhotos = async function (url) {
 };
 
 // const renderPhotos = function() {};
+btnShowMore.addEventListener('click', showMoreHandler);
 
 loadPhotos(
   `${API_URL}query=london&page=${page}&per_page=${ITEMS_PER_PAGE}&client_id=${API_KEY}`
 );
-console.log(portfolioGrid);
+// console.log(portfolioGrid);
