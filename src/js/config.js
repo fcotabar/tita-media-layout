5c48e7cdac49d70fcae1d1916f32191d456f52ba
fcotabar <fcotabar@gmail.com>
2021-06-26 19:15:55 -0500

    Responsive mobile menu

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.unsplash.com/search/photos?';
+const API_KEY = 'vodmIJuD9An8bl57-l_CkGeUBy8kCmDQ3sCZwihGr94';
+const ITEMS_PER_PAGE = 9;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,6 @@
-'use strict';
+///////////////////////
+// CONFIG
+//////////////////////
 
 const API_URL = 'https://api.unsplash.com/search/photos?';
 const API_KEY = 'vodmIJuD9An8bl57-l_CkGeUBy8kCmDQ3sCZwihGr94';
